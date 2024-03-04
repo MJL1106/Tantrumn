e0aadb2bc1b66477c1e781b3983a4b0bc8cf60ee
MJL1106 <75175998+MJL1106@users.noreply.github.com>
2024-03-04 18:57:42 +0000

    edited launch

--- LaunchServer.bat
+++ LaunchServer.bat
@@ -1,1 +1,1 @@
-"D:\Program Files (x86)\Epic Games\UE_4.25\Engine\Binaries\Win64\UE4Editor.exe" "D:\Unreal Projects\Tantrumn\Tantrumn.uproject" /Content/Tantrumn/Maps/Playground_OnlineMP -server -log
+"D:\Program Files (x86)\Epic Games\UE_4.25\Engine\Binaries\Win64\UE4Editor.exe" "D:\Unreal Projects\Tantrumn\Tantrumn.uproject" /Game/Tantrumn/Maps/Playground_OnlineMP -server -log
